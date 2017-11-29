24a3a467045146c11a8ee9f4cf57f26e004a36d2
d9pouces <github@19pouces.net>
2017-11-29 19:26:43 +0100

    clean setup.py files

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-import codecs
+import os.path
+import re
+
+from setuptools import setup, find_packages
 import os.path
 import re
 
 from setuptools import setup, find_packages
 
-
 version = None
-for line in codecs.open(os.path.join('autoplanner', '__init__.py'), 'r', encoding='utf-8'):
+for line in open(os.path.join('autoplanner', '__init__.py'), 'r', encoding='utf-8'):
     matcher = re.match(r"""^__version__\s*=\s*['"](.*)['"]\s*$""", line)
     version = version or matcher and matcher.group(1)
 
 # get README content from README.md file
-with codecs.open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as fd:
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as fd:
     long_description = fd.read()
-
-entry_points = {'console_scripts': ['autoplanner-manage = djangofloor.scripts:django',
-                                    'autoplanner-celery = djangofloor.scripts:celery',
-                                    'autoplanner-web = djangofloor.scripts:aiohttp']}
 
 setup(
     name='autoplanner',
@@ -27,17 +25,26 @@ setup(
     author_email='matthieu.gallet@19pouces.net',
     license='CeCILL-B',
     url='http://autoplanner.readthedocs.org/en/latest/',
-    entry_points=entry_points,
+    entry_points={'console_scripts': ['autoplanner-ctl = djangofloor.scripts:control']},
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     test_suite='autoplanner.tests',
-    install_requires=['djangofloor', 'icalendar', 'markdown', 'django', ],
+    install_requires=['djangofloor>=1.1.0', 'icalendar', 'markdown', 'django', ],
     setup_requires=[],
-    classifiers=['Development Status :: 3 - Alpha', 'Operating System :: MacOS :: MacOS X',
-                 'Operating System :: Microsoft :: Windows', 'Operating System :: POSIX :: BSD',
-                 'Operating System :: POSIX :: Linux', 'Operating System :: Unix',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Framework :: Django :: 1.11',
+                 'Framework :: Django :: 2.0',
+                 'Natural Language :: English',
+                 'Natural Language :: French',
+                 'Operating System :: MacOS :: MacOS X',
+                 'Operating System :: POSIX :: BSD',
+                 'Operating System :: POSIX :: Linux',
+                 'Operating System :: Unix',
                  'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
-                 'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3 :: Only'],
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3 :: Only'
+                 ],
 )
