6327d6253e3df915b3a34d65ca53851ce57dbc96
d9pouces <github@19pouces.net>
2018-04-21 00:44:37 +0200

    better handling of websockets

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from djangofloor.scripts import django, set_env
+from djangofloor.scripts import control, set_env
 
 __author__ = 'Matthieu Gallet'
 
 set_env(command_name='autoplanner-django')
-django()
+if __name__ == "__main__":
+    control()
